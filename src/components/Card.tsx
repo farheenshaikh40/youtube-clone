904b0c3f142fd035f3c20e424989548edbbca58b
farheenshaikh40 <farheenshaikh40@gmail.com>
2023-05-03 01:08:47 +0530

    removed comment

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,6 @@ import { HomePageVideos } from '../Types'
 import { Link } from 'react-router-dom'
 
 export default function Card({data}: {data:HomePageVideos}) {
-    // const isData = data ? true  : false
   return (
     <div className="w-64 h-60 flex gap-3 flex-col">
       <div className="relative">
